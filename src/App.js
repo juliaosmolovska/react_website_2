ba407ebaf41350f52f7338e949682ed20b0cd241
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-08 12:22:31 +0200

    Component for routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
+
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import {Route, Switch} from 'react-router-dom';
+
 function App() {
   return (
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/rooms/' component={Rooms} />
+      <Route exact path='/rooms/:slug' component={SingleRoom} />
     </>
   );
 }
