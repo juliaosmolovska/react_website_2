3442ed4d8aa16becbd3db5cbc22232a48b468618
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-08 16:44:57 +0200

    Nav Button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,10 +16,25 @@ export default class Navbar extends Component {
       <nav className='navbar'>
         <div className='nav-center'>
           <div className='nav-header'>
-           <Link to='/'>
-            <img src={logo} alt='Beach Resort' />
-           </Link>
+            <Link to='/'>
+              <img src={logo} alt='Beach Resort' />
+            </Link>
+            <button
+              type='button'
+              className='nav-btn'
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className='nav-icon' />
+            </button>
           </div>
+          <ul className={this.state.isOpen?'nav-links show-nav':'nav-links'}>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/rooms'>Rooms</Link>
+            </li>
+          </ul>
         </div>
       </nav>
     )
