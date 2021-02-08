015186f1bc399c6e3f1e10f35cd77f152cf5614e
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-08 12:34:53 +0200

    Switch

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/rooms/' component={Rooms} />
-      <Route exact path='/rooms/:slug' component={SingleRoom} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms/' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
