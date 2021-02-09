7c9c4937a07628567b9f4c58e2561554a8305f6c
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-09 15:15:46 +0200

    Services on Home page

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
 import Title from '../components/Title';
 
 export default class Services extends Component {
+  state={
+    services:[
+      {
+        icon: <FaCocktail></FaCocktail>,
+        title:'free cocktails',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        icon: <FaHiking></FaHiking>,
+        title:'endless hiking',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        icon: <FaShuttleVan></FaShuttleVan>,
+        title:'free shuttle',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        icon: <FaBeer></FaBeer>,
+        title:'strongest beer',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      }
+    ]
+  }
   render() {
     return (
-      <div>
+      <section className='services'>
         <Title title='services'></Title>
-      </div>
+        <div className='services-center'>
+          {this.state.services.map((item,index) => {
+            return (
+              <article key={index} className='service'>
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            )
+          })}
+        </div>
+      </section>
     )
   }
 }
