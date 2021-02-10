446c79f5e74a4049c6744902df5346046b60ec5d
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-10 16:07:17 +0200

    Add context

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import {  BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RoomProvider } from './context';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   document.getElementById('root')
 );
 
