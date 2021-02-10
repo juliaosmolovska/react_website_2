3b38668e09b2e95a1e4f5d7787f2209bb6f4caae
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-10 16:51:00 +0200

    Value in Home page

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { RoomContext } from '../context';
 
 export default class FeaturedRooms extends Component {
-  render() {
-    return (
-      <div>Hello from FeaturedRooms</div>
-    )
-  }
+  static contextType = RoomContext
+    render() {
+      const value = this.context;
+      console.log(value);
+      
+      return (
+        <div>Hello from FeaturedRooms {value}</div>
+      )
+    }
 }
