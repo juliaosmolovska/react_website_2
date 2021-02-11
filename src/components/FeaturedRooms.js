c42e9fb9e5e40c4af4b20add6258c0e9db470228
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-11 17:44:57 +0200

    Connect with Room

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
 import Loading from './Loading';
+import Room from './Room';
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
@@ -10,6 +11,7 @@ export default class FeaturedRooms extends Component {
       return (
         <div>
           Hello from FeaturedRooms
+          <Room></Room>
           <Loading></Loading>
         </div>
       )
