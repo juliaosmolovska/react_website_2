a7159ab417d4b20895682f77ed90cbb7ba9537f0
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-15 14:45:30 +0200

    Default images in SingleRoom (hero)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services';
 import FeaturedRooms from '../components/FeaturedRooms';
-import Button from '../components/StyledHero';
+import StyledHero from '../components/StyledHero';
 
 export default function Home() {
   return (
@@ -20,7 +20,6 @@ export default function Home() {
       </Hero>
       <Services></Services>
       <FeaturedRooms></FeaturedRooms>
-      <Button>hello</Button>
     </>
   )
 }
