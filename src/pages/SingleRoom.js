6c5a6a71ccc239be234446f83f167e2dd8b2b7ed
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-15 11:39:22 +0200

    Info about SingleRoom

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -28,10 +28,25 @@ export default class SingleRoom extends Component {
         </div>
       )
     }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
     return (
-      <div>
-        Hello from Single Room page{room.name}
-      </div>
+      <Hero hero='roomsHero'>
+        <Banner title={`${name} room` }>
+          <Link to='/rooms' className='btn-primary'>
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
     )
   }
 }
