ac523b28fc37a7fe88083fbce505ebf6030f72bc
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-17 16:18:58 +0200

    Room Type Filter on Rooms page

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -5,10 +5,36 @@ import Title from './Title';
 
 export default function RoomFilter() {
   const context = useContext(RoomContext);
-  console.log(context);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+  
   return (
-    <div>
-      Hello from RoomFilter
-    </div>
-  )
+    <section className='filter-container'>
+      <Title title='search rooms'></Title>
+        <form className='filter-form'>
+          {/* select type */}
+          <div className='form-group'>
+            <label htmlFor='type'>room type</label>
+            <select
+              name='type'
+              id='type'
+              value={type}
+              className='form-control'
+              onChange={handleChange}
+            />
+          </div>
+          {/* end select type */}
+        </form>
+    </section>
+  );
 }
