e0fcabc55629dad2a13847ebbe8285962e6fa126
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-18 12:48:38 +0200

    Add Types for RoomType

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -3,7 +3,11 @@ import { useContext } from 'react';
 import { RoomContext } from '../context';
 import Title from './Title';
 
-export default function RoomFilter() {
+//get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+}
+export default function RoomFilter({rooms}) {
   const context = useContext(RoomContext);
   const {
     handleChange,
@@ -17,7 +21,14 @@ export default function RoomFilter() {
     breakfast,
     pets
   } = context;
-  
+  //get unique types
+  let types = getUnique(rooms, 'type');
+  //add all
+  types = ['all',...types];
+  //map to jsx
+  types = types.map((item, index) => {
+    return <option value={item} key={index}>{item}</option>;
+  })
   return (
     <section className='filter-container'>
       <Title title='search rooms'></Title>
@@ -31,7 +42,9 @@ export default function RoomFilter() {
               value={type}
               className='form-control'
               onChange={handleChange}
-            />
+            >
+              {types}
+            </select>
           </div>
           {/* end select type */}
         </form>
